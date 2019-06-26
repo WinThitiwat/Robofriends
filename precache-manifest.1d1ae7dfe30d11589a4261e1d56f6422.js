b4668b76498c5d9ff76cdc02e915bcfa02e4506d
Win <win.thitiwat@gmail.com>
2019-06-25 17:38:32 -0700

    Updates

---
+++ precache-manifest.1d1ae7dfe30d11589a4261e1d56f6422.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96b3beaeeb316ad3e9a3bbe41cc046f",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "72ffb2b2e19b20bc102e",
+    "url": "/Robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "00b2d25a421ba74f46ce",
+    "url": "/Robofriends/static/css/main.dba525f8.chunk.css"
+  },
+  {
+    "revision": "72ffb2b2e19b20bc102e",
+    "url": "/Robofriends/static/js/2.85a16621.chunk.js"
+  },
+  {
+    "revision": "00b2d25a421ba74f46ce",
+    "url": "/Robofriends/static/js/main.92545c45.chunk.js"
+  },
+  {
+    "revision": "47afcf7c1fdce8fc928a",
+    "url": "/Robofriends/static/js/runtime~main.4fea8ee0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
